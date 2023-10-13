ed2e34fe677dd33fae2027f5cf8130b1e967791a
Jonah George <jonah.george@getampla.com>
2023-10-13 13:50:43 -0700

    Fix Django 4.1 deprecation warning

--- src/rest_framework_jwt/blacklist/__init__.py
+++ src/rest_framework_jwt/blacklist/__init__.py
@@ -1,1 +1,4 @@
-default_app_config = 'rest_framework_jwt.blacklist.apps.BlacklistedTokenConfig'
+import django
+
+if django.VERSION < (3, 2):
+  default_app_config = 'rest_framework_jwt.blacklist.apps.BlacklistedTokenConfig'
